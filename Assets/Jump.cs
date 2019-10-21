bdbe5d970e8359f00c0c09a3e1423cd5b59c6d5c
Kevin T'Syen <kevin.tsyen@gmail.com>
2019-10-21 13:29:49 +0200

    Movement + Jump

---
+++ Assets/Jump.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public bool isJumpPressed;
+    public Rigidbody2D rb;
+    public float jumpSpeed = 1000f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            isJumpPressed = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isJumpPressed)
+        {
+            Debug.Log("Jump!");
+            isJumpPressed = false;
+            rb.AddForce(Vector2.up * (jumpSpeed * Time.fixedDeltaTime));
+        }
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public bool isMovingLeft;
+    public bool isMovingRight;
+    public Rigidbody2D rb;
+    public float movementSpeed = 500f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            isMovingLeft = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            isMovingRight = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (isMovingLeft)
+        {
+            Debug.Log("Move left!");
+            /*rb.AddForce(Vector2.left * (movementSpeed * Time.fixedDeltaTime));*/
+            rb.velocity = new Vector2((-1 * movementSpeed) * Time.fixedDeltaTime, 0);
+            isMovingLeft = false;
+        }
+        if (isMovingRight)
+        {
+            Debug.Log("Move right!");
+            rb.velocity = new Vector2((1 * movementSpeed) * Time.fixedDeltaTime, 0);
+            isMovingRight = false;
+        }
+    }
+}
+
